c6914e28456185850983d8070a06d6610e816f37
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-09-09 13:23:20 +0200

    Added image loading to sample

--- src/Sample/Views/MainActivity.cs
+++ src/Sample/Views/MainActivity.cs
@@ -2,18 +2,24 @@
 using Android.OS;
 using Android.Util;
 using Android.Views;
+using Android.Widget;
+using Bumptech;
+using Bumptech.Request;
+using Bumptech.Request.Target;
 using IN.Srain.Cube.Views.Ptr;
+using System;
 
 namespace Sample.Views
 {
     [Activity(
         Label = "Sample",
         MainLauncher = true)]
-    public class MainActivity : Activity, IPtrHandler
+    public class MainActivity : Activity, IPtrHandler, IRequestListener
     {
-        private string mUrl = "http://img4.duitang.com/uploads/blog/201407/07/20140707113856_hBf3R.thumb.jpeg";
-        private long mStartLoadingTime = -1;
-        private bool mImageHasLoaded = false;
+        private int currentImageIndex = -1;
+
+        private ImageView imageView;
+        private PtrFrameLayout refreshFrame;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,26 +27,27 @@ namespace Sample.Views
 
             SetContentView(Resource.Layout.activity_main);
 
-            //CubeImageView imageView = (CubeImageView)view.findViewById(R.id.material_style_image_view);
-            //ImageLoader imageLoader = ImageLoaderFactory.create(getContext());
+            imageView = FindViewById<ImageView>(Resource.Id.material_style_image_view);
+            refreshFrame = FindViewById<PtrFrameLayout>(Resource.Id.material_style_ptr_frame);
 
-            PtrFrameLayout frame = FindViewById<PtrFrameLayout>(Resource.Id.material_style_ptr_frame);
-
-            // header
-            RentalsSunHeaderView header = new RentalsSunHeaderView(this);
-            header.LayoutParameters = new PtrFrameLayout.LayoutParams(-1, -2);
+            var header = new RentalsSunHeaderView(this)
+            {
+                LayoutParameters = new PtrFrameLayout.LayoutParams(-1, -2)
+            };
             header.SetPadding(0, (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, 15, Resources.DisplayMetrics), 0, (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, 10, Resources.DisplayMetrics));
-            header.SetUp(frame);
+            header.SetUp(refreshFrame);
 
-            frame.SetLoadingMinTime(1000);
-            frame.SetDurationToCloseHeader(1500);
-            frame.HeaderView = header;
-            frame.AddPtrUIHandler(header);
+            refreshFrame.SetLoadingMinTime(1000);
+            refreshFrame.SetDurationToCloseHeader(1500);
+            refreshFrame.HeaderView = header;
+            refreshFrame.AddPtrUIHandler(header);
 
-            frame.PostDelayed(() => frame.AutoRefresh(true), 100);
+            refreshFrame.PostDelayed(() => refreshFrame.AutoRefresh(true), 100);
 
-            frame.SetPtrHandler(this);
+            refreshFrame.SetPtrHandler(this);
         }
+
+        #region Pull to refresh callbacks
 
         public bool CheckCanDoRefresh(PtrFrameLayout frame, View content, View header)
         {
@@ -49,8 +56,46 @@ namespace Sample.Views
 
         public void OnRefreshBegin(PtrFrameLayout frame)
         {
-            frame.PostDelayed(() => frame.RefreshComplete(), 1500);
+            Glide.With(this).Load(GetImageUrl()).Listener(this).Into(imageView);
         }
+
+        #endregion Pull to refresh callbacks
+
+        #region Image Loading callbacks
+
+        public bool OnException(Java.Lang.Exception p0, Java.Lang.Object p1, ITarget p2, bool p3)
+        {
+            Toast.MakeText(this, Resource.String.error_showing_image, ToastLength.Long)
+                .Show();
+
+            return false;
+        }
+
+        public bool OnResourceReady(Java.Lang.Object p0, Java.Lang.Object p1, ITarget p2, bool p3, bool p4)
+        {
+            refreshFrame.RefreshComplete();
+            return false;
+        }
+
+        #endregion Image Loading callbacks
+
+        #region Helpers
+
+        private string GetImageUrl()
+        {
+            var random = new Random();
+
+            int nextIndex = currentImageIndex;
+
+            while (nextIndex == currentImageIndex)
+                nextIndex = random.Next(0, 10);
+
+            currentImageIndex = nextIndex;
+
+            var urls = Resources.GetStringArray(Resource.Array.image_urls);
+            return urls[currentImageIndex];
+        }
+
+        #endregion Helpers
     }
 }
-
